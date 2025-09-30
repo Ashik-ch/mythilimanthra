2e3814f754683ab9edca0b1f9fbc8778eff81536
Ashik-ch <ashikch7117@gmail.com>
2025-09-30 12:03:57 +0530

    Remove unused imports from App.tsx to streamline code

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,3 @@
-import { useMemo, useState } from 'react'
-import { motion, AnimatePresence } from 'framer-motion'
-import type { Container, ISourceOptions } from 'tsparticles-engine'
 import data from './content.json'
 import { Footer } from './components/Footer';
 import { Hero } from './components/Hero'
